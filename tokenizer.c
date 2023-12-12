ec631785b4b3d9570ff0d4820806e0cbc0c6b4a3
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-12-12 11:12:34 -0800

    Added a tokenizer function

---
+++ tokenizer.c
@@ -0,0 +1,48 @@
+#include "cj_shell.h"
+
+/**
+ * tokenize - Splits a string into an array of tokens based on a delimeter
+ * @str: THe input string to be tokenized
+ * @delim: THe delimeter used to split the string
+ *
+ * Return: An array of strings 9tokens) or NULL if input is invalid
+ */
+
+char **tokenize(char *str, const char *delim)
+{
+	char **commands = NULL, *token, *dup_str;
+	size_t n_tokens, i;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	dup_str = strdup(str);
+	token = strtok(dup_str, delim);
+
+	n_tokens = 0;
+	while (token != NULL)
+	{
+		++n_tokens;
+		token = strtok(NULL, delim);
+	}
+	free(dup_str);
+
+	if (n_tokens > 0)
+	{
+		commands = malloc(sizeof(char *) * (n_tokens + 1));
+		if (commands == NULL)
+			exit(EXIT_FAILURE);
+
+		token = strtok(str, delim);
+		i = 0;
+
+		while (token != NULL)
+		{
+			commands[i] = strdup(token);
+			token = strtok(NULL, delim);
+			i++;
+		}
+		commands[i] = NULL;
+	}
+	return (commands);
+}
