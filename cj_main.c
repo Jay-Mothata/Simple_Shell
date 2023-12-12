e9fe9488a503b778a5b5f7045f297ba7f270b8d3
Cleveland Ataafi <thatguykwamec@gmail.com>
2023-12-12 11:11:54 -0800

    Edits to main file

--- cj_main.c
+++ cj_main.c
@@ -2,26 +2,48 @@
 
 /**
  * main - Entry point for the simple shell
- *
- * Return: Always returns EXIT_SUCCESS
+ * @argc: Number of command-line arguements
+ * @argv: Array of command-line arguements
+ * Return: Always 0 (success)
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	char *cmd;
+	char *lineptr = NULL, **commands = NULL;
+	size_t n = 0, cmd_count = 0;
+	ssize_t n_read;
 
+	(void)argc, (void)argv;
 	while (true)
 	{
-		dispwait_prompt();
-		cmd = reads_from_user();
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		fflush(stdout);
+		n_read = getline(&lineptr, &n, stdin);
+		++cmd_count;
 
-		if (cmd != NULL && *cmd != '\0')
+		if (n_read == -1)
 		{
-			executes_cmd(cmd);
+			free(lineptr);
+			lineptr = NULL;
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			exit(0);
 		}
+		lineptr[n_read - 1] = '\0';
 
-		free(cmd); /*Frees memory allocated for user input */
+		if (*lineptr == '\0')
+			continue;
+
+		commands = tokenize(lineptr, " ");
+		if (commands == NULL)
+			continue;
+
+		exec_cmds(lineptr, commands);
+		free(lineptr);
+		free_cmds(commands);
+		commands = NULL;
+		lineptr = NULL;
 	}
-
-	return (EXIT_SUCCESS);
+	return (0);
 }
