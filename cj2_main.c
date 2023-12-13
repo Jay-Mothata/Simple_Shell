86c8dbafa5c688512c3574bc71205903131c4032
jay-mothata <joey.mothata@gmail.com>
2023-12-13 01:38:18 -0800

    Handles the 'exit' command as a built-in

--- cj2_main.c
+++ cj2_main.c
@@ -1,47 +1,59 @@
 #include "cj_shell.h"
 
 /**
+ * handle_user_input - Prompts the user for input and reads the command line.
+ * @lineptr: Pointer to the input string.
+ * @len: Length of the input string.
+ */
+
+void handle_user_input(char **lineptr, size_t *len)
+{
+	cj_print("Shell$ ");
+	getline(lineptr, len, stdin);
+	if ((*lineptr)[strlen(*lineptr) - 1] == '\n')
+		(*lineptr)[strlen(*lineptr) - 1] = '\0';
+}
+
+/**
+ * process_command - Tokenizes and executes the user-entered command.
+ * @lineptr: Pointer to the input string.
+ */
+
+void process_command(char *lineptr)
+{
+	char **commands = tokenize(lineptr, " ");
+
+	if (commands != NULL && commands[0] != NULL)
+	{
+		if (strcmp(commands[0], "exit") == 0)
+		{
+			free_cmds(commands);
+			cj_exit();
+		}
+
+		exec_cmds(lineptr, commands);
+		free_cmds(commands);
+	} else
+	{
+		cj_print("Invalid input.\n");
+	}
+}
+
+/**
  * main - Entry point for the simple shell program.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	/* Variables for input */
 	char *lineptr = NULL;
 	size_t len = 0;
 
-	/* Shell main loop */
 	while (1)
 	{
-		cj_print("Shell$ ");
-		getline(&lineptr, &len, stdin);
-
-		/* Remove the newline character at the end */
-		if (lineptr[strlen(lineptr) - 1] == '\n')
-			lineptr[strlen(lineptr) - 1] = '\0';
-
-		/* Tokenize the input command */
-		char **commands = tokenize(lineptr, " ");
-
-		if (commands != NULL && commands[0] != NULL)
-		{
-			/* Check for built-in commands */
-			if (strcmp(commands[0], "exit") == 0)
-			{
-				free_cmds(commands);
-				cj_exit();
-			}
-
-			/* Execute the command */
-			exec_cmds(lineptr, commands);
-			free_cmds(commands);
-		}
-		else
-		{
-			cj_print("Invalid input.\n");
-		}
-
+		handle_user_input(&lineptr, &len);
+		process_command(lineptr);
 		free(lineptr);
 		lineptr = NULL;
 	}
