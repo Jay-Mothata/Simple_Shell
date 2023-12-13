0d492350884eb66279a501b78010b669dbf61a87
jay-mothata <joey.mothata@gmail.com>
2023-12-13 14:06:58 -0800

    Edit to file

--- exec_cmds.c
+++ exec_cmds.c
@@ -11,8 +11,8 @@ void exec_cmds(char *lineptr, char *argv[])
 	pid_t child;
 	int status;
 
-	/*Get the full path of the command using get_path */
-	get_path(&argv[0]);
+	/*Get the full path of the command using get_path
+	get_path(&argv[0]); */
 
 	if (access(argv[0], X_OK | R_OK) == -1)
 	{
@@ -30,7 +30,7 @@ void exec_cmds(char *lineptr, char *argv[])
 		if (execve(argv[0], argv, environ) == -1)
 		{
 			perror("execve");
-			_exit(EXIT_FAILURE);
+			/* _exit(EXIT_FAILURE); */
 		}
 	}
 	else
